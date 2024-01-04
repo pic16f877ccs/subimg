dd643d822a4ad85e9bb0d0a69525ea96bf0f1748
Karl Wulfert <pic16f877.ccs@emailn.de>
2024-01-04 22:58:30 +0100

    Update version to v0.3.0

--- src/main.rs
+++ src/main.rs
@@ -1,258 +1,252 @@
-use cev::Cev;
-use clap::{crate_version, value_parser, Arg, ArgMatches, Command};
-use image::{open, save_buffer, ColorType, DynamicImage, ImageFormat, ImageResult};
-use std::error;
+use anyhow::anyhow;
+use anyhow::Context;
+use clap::{crate_name, crate_version, value_parser, Arg, ArgMatches, Command};
+use image::{open, save_buffer, ColorType, ImageFormat};
 use std::path::PathBuf;
 
+const EXTRACT_ERR: &str = "Failed to extract integrated image";
 type Size = (u32, u32);
-type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-#[derive(Default)]
 struct ImgInImg {
-    img_data: Vec<u8>,
-    img_size: Size,
-    img_alpha: Option<ColorType>,
-    sub_img_data: Option<Vec<u8>>,
-    sub_img_size: Size,
-    sub_img_len: Option<usize>,
+    basic_img_data: Vec<u8>,
+    basic_img_size: Size,
+    other_img_data: Vec<u8>,
+    other_img_size: Size,
 }
 
 impl ImgInImg {
-    fn new() -> ImgInImg {
-        ImgInImg::default()
+    fn open_basic_image(app: &ArgMatches) -> anyhow::Result<ImgInImg> {
+        let Some(path) = app.get_one::<PathBuf>("image") else {
+            panic!()
+        };
+        let basic_img = open(path)
+            .with_context(|| format!("Failed to read image file from {}", path.display()))?;
+        if basic_img.color().has_alpha() {
+            Ok(ImgInImg {
+                basic_img_size: (basic_img.width(), basic_img.height()),
+                basic_img_data: basic_img.into_rgba8().into_vec(),
+                other_img_size: (0u32, 0u32),
+                other_img_data: vec![],
+            })
+        } else {
+            Err(anyhow!(
+                "An image file without an alpha channel is not supported"
+            ))
+        }
     }
 
-    fn open_image(&mut self, app: &ArgMatches) -> ImageResult<()> {
-        let path = app
-            .get_one::<PathBuf>("image")
-            .map(|path| path.to_path_buf())
-            .expect("internal error opening file");
-        let img = open(path)?;
-        self.img_alpha = match img.color() {
-            ColorType::Rgba8 => Some(ColorType::Rgba8),
-            _ => None,
-        };
-        self.img_size = (img.width(), img.height());
-        self.img_data = img.into_rgba8().into_vec();
-        self.sub_img_size = self.sub_img_size();
-        self.sub_img_len = self.sub_img_len();
-        self.sub_img_data = self.sub_img_data();
+    fn open_other_image(&mut self, app: &ArgMatches) -> anyhow::Result<()> {
+        if let Some(path) = app.get_one::<PathBuf>("input") {
+            let other_img = open(path)
+                .with_context(|| format!("Failed to read image file from {}", path.display()))?;
+            self.other_img_size = (other_img.width(), other_img.height());
+            self.other_img_data = other_img.into_rgb8().into_vec();
+            self.merge_img_datas(app)?;
+        }
         Ok(())
     }
 
-    fn sub_img_size(&self) -> Size {
-        let mut encoded_size = Vec::from(&self.img_data[..10]);
-        encoded_size.remove(9);
-        encoded_size.remove(4);
-        let mut width_buf = [0, 0, 0, 0];
-        let mut height_buf = [0, 0, 0, 0];
-        width_buf.clone_from_slice(&encoded_size[..4]);
-        height_buf.clone_from_slice(&encoded_size[4..8]);
-        (
-            u32::from_ne_bytes(width_buf),
-            u32::from_ne_bytes(height_buf),
-        )
-    }
+    fn merge_img_datas(&mut self, app: &ArgMatches) -> anyhow::Result<()> {
+        let size_as_bytes = self.size_to_bytes();
+        let mut other_iter = self.other_img_data.iter();
+        let basic_iter = self.basic_img_data.chunks_mut(4).filter_map(|chunk| {
+            if chunk[3] == 0 {
+                Some(&mut chunk[..3])
+            } else {
+                None
+            }
+        });
 
-    fn sub_img_len(&self) -> Option<usize> {
-        let Some(sub_img_len) = self.sub_img_size.0.checked_mul(self.sub_img_size.1) else {
-            return None;
-        };
-        let Some(sub_img_len) = sub_img_len.checked_mul(4) else {
-            return None;
-        };
-        let Some(sub_img_len) = sub_img_len.checked_add(12) else {
-            return None;
-        };
-        if sub_img_len <= 12 {
-            return None;
-        };
-        Some(sub_img_len as usize)
-    }
-
-    fn sub_img_data(&mut self) -> Option<Vec<u8>> {
-        let Some(sub_img_len) = self.sub_img_len else {
-            return None;
-        };
-
-        let sub_img_data = self
-            .img_data
-            .chunks_mut(4)
-            .filter(|chunk| chunk[3] == 0)
-            .take(sub_img_len / 4)
-            .flat_map(|chunk| {
-                if chunk[0..3] == [0, 0, 0] {
-                    [255, 255, 255, 255]
-                } else {
-                    chunk[3] = 255;
-                    [chunk[0], chunk[1], chunk[2], chunk[3]]
+        if app.get_flag("fill") {
+            for chunk in basic_iter {
+                for basic_elem in chunk.iter_mut() {
+                    if let Some(other_elem) = other_iter.next() {
+                        *basic_elem = *other_elem;
+                    } else {
+                        other_iter = self.other_img_data.iter();
+                    };
                 }
-            })
-            .collect::<Vec<_>>();
+            }
+        } else {
+            let mut size_other_iter = size_as_bytes.iter().chain(other_iter);
+            'outer: for chunk in basic_iter {
+                for basic_elem in chunk.iter_mut() {
+                    let Some(size_other_elem) = size_other_iter.next() else {
+                        break 'outer;
+                    };
+                    *basic_elem = *size_other_elem;
+                }
+            }
 
-        if sub_img_data.len() != sub_img_len {
-            return None;
-        }
-        Some(sub_img_data)
-    }
-
-    fn subvec_to_vec(&mut self, sub_vec: Vec<u8>) -> Result<()> {
-        let sub_vec_len = sub_vec.len() / 4;
-        let mut sub_iter = sub_vec.iter();
-        let iter = self
-            .img_data
-            .chunks_mut(4)
-            .filter(|chunk| chunk[3] == 0)
-            .take(sub_vec_len);
-
-        for chunk in iter {
-            for elem in chunk.iter_mut() {
-                let Some(sub_elem) = sub_iter.next() else {
-                    return Err("the inner error of writing a sub-vector into a vector".into());
-                };
-                *elem = *sub_elem;
+            if size_other_iter.next().is_some() {
+                return Err(anyhow!("There is not enough free space in the image"));
             }
         }
 
-        if sub_iter.next().is_some() {
-            return Err("there is not enough free space in the image".into());
-        };
         Ok(())
+    }
+
+    fn save_basic_image(&self, app: &ArgMatches) -> anyhow::Result<()> {
+        if let Some(path) = app.get_one::<PathBuf>("output") {
+            let format = ImageFormat::from_path(path)?;
+            if app.get_flag("rgb") && (ImageFormat::Png == format || ImageFormat::Tiff == format) {
+                save_buffer(
+                    path,
+                    &self.rgba_to_rgb(),
+                    self.basic_img_size.0,
+                    self.basic_img_size.1,
+                    ColorType::Rgb8,
+                )
+                .with_context(|| {
+                    format!(
+                        "The image could not be written to the file {}",
+                        path.display()
+                    )
+                })?;
+                return Ok(());
+            }
+
+            save_buffer(
+                path,
+                &self.basic_img_data,
+                self.basic_img_size.0,
+                self.basic_img_size.1,
+                ColorType::Rgba8,
+            )
+            .with_context(|| {
+                format!(
+                    "The image could not be written to the file {}",
+                    path.display()
+                )
+            })?;
+        }
+        Ok(())
+    }
+
+    fn detach_img_data(&mut self) -> anyhow::Result<Vec<u8>> {
+        let mut detach_data_iter = self
+            .basic_img_data
+            .chunks(4)
+            .filter_map(|chank| {
+                if chank[3] == 0 {
+                    Some(&chank[0..3])
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .cloned();
+
+        self.other_img_size = (
+            iter_to_size(&mut detach_data_iter)?,
+            iter_to_size(&mut detach_data_iter)?,
+        );
+        let other_len = (self.other_img_size.0 * self.other_img_size.1) as usize * 3;
+        let other_img_data = detach_data_iter.take(other_len).collect::<Vec<u8>>();
+        if other_len != other_img_data.len() || other_img_data.len() < 12 {
+            return Err(anyhow!(EXTRACT_ERR));
+        }
+
+        Ok(other_img_data)
+    }
+
+    fn save_other_image(&mut self, app: &ArgMatches) -> anyhow::Result<()> {
+        if let Some(path) = app.get_one::<PathBuf>("output_subimg") {
+            let color_type = ColorType::Rgb8;
+            self.other_img_data = self.detach_img_data()?;
+
+            save_buffer(
+                path,
+                &self.other_img_data,
+                self.other_img_size.0,
+                self.other_img_size.1,
+                color_type,
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn size_to_bytes(&self) -> [u8; 8] {
+        let w = self.other_img_size.0.to_ne_bytes();
+        let h = self.other_img_size.1.to_ne_bytes();
+        [w[0], w[1], w[2], w[3], h[0], h[1], h[2], h[3]]
+    }
+
+    fn rgba_to_rgb(&self) -> Vec<u8> {
+        self.basic_img_data
+            .chunks(4)
+            .flat_map(|chank| &chank[0..3])
+            .cloned()
+            .collect::<Vec<u8>>()
     }
 
     fn available_pixels(&self, app: &ArgMatches) {
         if app.get_flag("pixels") {
-            if self.img_alpha.is_some() {
-                let pixels = self
-                    .img_data
-                    .chunks(4)
-                    .filter(|chunk| chunk[3] == 0)
-                    .count()
-                    / 1_000_000;
+            let pixels = self
+                .basic_img_data
+                .chunks(4)
+                .filter(|chunk| chunk[3] == 0)
+                .count();
 
-                println!("\n {} megapixels available in the image", pixels);
-            } else {
-                println!("\n there are no available pixels in the image");
-            }
+            println!("{} pixels available in the image", pixels);
         }
-    }
-
-    fn add_sub_img_data(&mut self, path: &PathBuf) -> Result<()> {
-        let sub_img = open_sub_img(path)?;
-        let sub_img_size = encode_sub_img_size(&sub_img);
-        let sub_img_data = sub_img.into_rgba8().into_vec();
-        let mut tmp_data = Cev::from_vec(sub_img_data);
-        tmp_data.append(&mut Cev::from(sub_img_size));
-        let mut sub_img_data = tmp_data.into_vec();
-        img_to_invisible(&mut sub_img_data);
-        self.subvec_to_vec(sub_img_data)?;
-        Ok(())
-    }
-
-    fn all_alpha_max(&mut self, app: &ArgMatches) {
-        if app.get_flag("all") {
-            self.img_data
-                .iter_mut()
-                .skip(3)
-                .step_by(4)
-                .for_each(|alpha| {
-                    *alpha = 255;
-                });
-        }
-    }
-
-    fn save_img_in_img(&mut self, app: &ArgMatches) -> Result<()> {
-        if let Some(path) = app
-            .get_one::<PathBuf>("input")
-            .map(|path| path.to_path_buf())
-        {
-            let Some(_) = self.img_alpha else {
-                return Err("Unsupported image input format. Try `tiff` or `png` format.".into());
-            };
-            self.add_sub_img_data(&path)?;
-        }
-        Ok(())
-    }
-
-    fn save_sub_img(&self, app: &ArgMatches) -> Result<()> {
-        if let Some(path) = app.get_one::<PathBuf>("output_subimg") {
-            let Some(ref img) = self.sub_img_data else {
-                return Err("subimage decoding error".into());
-            };
-            let color_type = ColorType::Rgba8;
-            let size = self.sub_img_size;
-            save_buffer(path, &img[12..], size.0, size.1, color_type)?;
-        }
-        Ok(())
-    }
-
-    fn save_img(&self, app: &ArgMatches) -> Result<()> {
-        if let Some(path) = app.get_one::<PathBuf>("output") {
-            let color_type = ColorType::Rgba8;
-            let format = ImageFormat::from_path(path)?;
-            if app.contains_id("input") {
-                let (ImageFormat::Png | ImageFormat::Tiff) = format else {
-                    return Err(
-                        "Unsupported image output format. Try `tiff` or `png` format.".into(),
-                    );
-                };
-            }
-            save_buffer(
-                path,
-                &self.img_data,
-                self.img_size.0,
-                self.img_size.1,
-                color_type,
-            )?;
-        }
-        Ok(())
     }
 }
 
-fn open_sub_img(path: &PathBuf) -> ImageResult<DynamicImage> {
-    open(path)
+fn iter_to_size(iter: &mut impl Iterator<Item = u8>) -> anyhow::Result<u32> {
+    let Some(byte0) = iter.next() else {
+        return Err(anyhow!(EXTRACT_ERR));
+    };
+    let Some(byte1) = iter.next() else {
+        return Err(anyhow!(EXTRACT_ERR));
+    };
+    let Some(byte2) = iter.next() else {
+        return Err(anyhow!(EXTRACT_ERR));
+    };
+    let Some(byte3) = iter.next() else {
+        return Err(anyhow!(EXTRACT_ERR));
+    };
+    Ok(u32::from_ne_bytes([byte0, byte1, byte2, byte3]))
 }
 
-fn encode_sub_img_size(sub_img: &DynamicImage) -> Vec<u8> {
-    let mut size = Vec::from(sub_img.width().to_ne_bytes());
-    size.insert(3, 0);
-    size.append(&mut Vec::from(sub_img.height().to_ne_bytes()));
-    size.insert(7, 0);
-    size.append(&mut vec![0, 0]);
-    size
-}
-
-fn img_to_invisible(img_data: &mut [u8]) {
-    img_data.iter_mut().skip(3).step_by(4).for_each(|alpha| {
-        *alpha = 0;
-    });
-}
-
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     let app = app_commands();
-    let mut img_in_img = ImgInImg::new();
-    img_in_img.open_image(&app)?;
+    let mut img_in_img = ImgInImg::open_basic_image(&app)?;
+    img_in_img.open_other_image(&app)?;
     img_in_img.available_pixels(&app);
-    img_in_img.save_sub_img(&app)?;
-    img_in_img.save_img_in_img(&app)?;
-    img_in_img.all_alpha_max(&app);
-    img_in_img.save_img(&app)?;
+    img_in_img.save_other_image(&app)?;
+    img_in_img.save_basic_image(&app)?;
+
     Ok(())
 }
 
 fn app_commands() -> ArgMatches {
-    Command::new("subimg")
+    Command::new(crate_name!())
         .about("A tool to hide sub-images in the image")
+        .after_help(
+            "Example:
+  subimg input.png -i subimage.png -o output.png",
+        )
         .long_version(crate_version!())
-        .author("    by PIC16F877ccs")
         .args_override_self(true)
         .arg(
             Arg::new("image")
                 .value_parser(value_parser!(PathBuf))
-                .value_name("PAPH")
-                .help("Path to input image file")
+                .value_name("PATH")
+                .help("Path to the input image file")
                 .index(1)
                 .required(true),
+        )
+        .arg(
+            Arg::new("fill")
+                .short('f')
+                .long("fill")
+                .requires("rgb")
+                .action(clap::ArgAction::SetTrue)
+                .num_args(0)
+                .help("Fill the transparent area with another image")
+                .required(false),
         )
         .arg(
             Arg::new("pixels")
@@ -260,35 +254,35 @@ fn app_commands() -> ArgMatches {
                 .long("pixels")
                 .action(clap::ArgAction::SetTrue)
                 .num_args(0)
-                .help("Available pixels in image")
+                .help("Check the available pixels in the image")
+                .required(false),
+        )
+        .arg(
+            Arg::new("rgb")
+                .short('r')
+                .long("no-alpha")
+                .action(clap::ArgAction::SetTrue)
+                .num_args(0)
+                .help("Save the image in PNG or TIFF format without an alpha channel")
                 .required(false),
         )
         .arg(
             Arg::new("input")
                 .short('i')
                 .long("input")
-                .value_name("PAPH")
+                .value_name("PATH")
                 .help("Path to sub image file")
                 .value_parser(value_parser!(PathBuf))
                 .num_args(1)
                 .required(false),
         )
         .arg(
-            Arg::new("all")
-                .short('a')
-                .long("all")
-                .action(clap::ArgAction::SetTrue)
-                .num_args(0)
-                .help("All pixels are visible")
-                .required(false),
-        )
-        .arg(
             Arg::new("output_subimg")
                 .short('O')
-                .conflicts_with_all(["output", "input", "all"])
+                .conflicts_with_all(["output", "input"])
                 .long("output-subimage")
-                .value_name("PAPH")
-                .help("Output sub image file")
+                .value_name("PATH")
+                .help("Output path for the sub-image file")
                 .value_parser(value_parser!(PathBuf))
                 .num_args(1)
                 .required(false),
@@ -297,8 +291,8 @@ fn app_commands() -> ArgMatches {
             Arg::new("output")
                 .short('o')
                 .long("output")
-                .value_name("PAPH")
-                .help("Output image file")
+                .value_name("PATH")
+                .help("Output path for the final image")
                 .value_parser(value_parser!(PathBuf))
                 .num_args(1)
                 .required(false),
